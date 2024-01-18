f92ea7d8586a1ef7dfea168fe07779839ea697b6
Александр Чернобров <alesandro83@mail.ru>
2024-01-18 15:38:53 +0300

    Выполнил стилизацию декоративных элементов

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
-/* в этот файл добавляет скрипты*/
+const headerToggle = document.querySelector('.header__toggle');
+const headerNav = document.querySelector('.header__nav');
+
+headerNav.classList.add('header__nav--none');
+
+headerToggle.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  headerToggle.classList.toggle('header__toggle--close');
+  headerNav.classList.toggle('header__nav--none');
+});
