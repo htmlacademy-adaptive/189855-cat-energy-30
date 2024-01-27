676562e43b742021e14a97ea63de71c400f16ee9
Александр Чернобров <alesandro83@mail.ru>
2024-01-27 17:25:48 +0300

    Убрал комментарии в js файле

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,8 +4,6 @@ const headerNav = document.querySelector('.header__nav');
 headerNav.classList.add('header__nav--none');
 headerNav.classList.remove('header__nav--static');
 headerToggle.classList.remove('header__toggle--none');
-// headerNav.classList.add('header__nav--static');
-// headerNav.classList.remove('header__nav--static');
 
 headerToggle.addEventListener('click', function(evt) {
   evt.preventDefault();
